f4fbf5514cf47f1f5bbfcfe970e4ef39ec5fc4d4
Pierre Prinetti <pierreprinetti@redhat.com>
2022-03-25 15:58:04 +0100

    First commit
    Script adapted from https://github.com/openshift/installer/blob/ca8aacd3c931eefabfddafb49cb44b76f24653a6/docs/user/openstack/invalid-https-certificates.md

---
+++ certcheck.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+declare -r url="$1"
+declare \
+	host='' \
+	port='' \
+	san='' \
+	noschema=''
+san="$(mktemp)"
+readonly san
+
+trap "rm -f '$san'" EXIT
+
+# Ignore HTTP endpoints
+if [[ ${url#"http://"} != "$url" ]]; then
+	echo "PASS: ${url} is not an HTTPS endpoint"
+fi
+
+# Remove the schema from the URL
+noschema=${url#"https://"}
+
+# Remove the path and only keep host and port
+noschema="${noschema%%/*}"
+host="${noschema%%:*}"
+port="${noschema##*:}"
+
+# Add the port if was implicit
+if [[ "$port" == "$host" ]]; then
+	port='443'
+fi
+
+# Get the SAN fields
+openssl s_client -showcerts -servername "$host" -connect "$host:$port" </dev/null 2>/dev/null \
+	| openssl x509 -noout -ext subjectAltName \
+	> "$san"
+
+# openssl returns the empty string if no SAN is found.
+# If a SAN is found, openssl is expected to return something like:
+#
+#    X509v3 Subject Alternative Name:
+#        DNS:standalone, DNS:osp1, IP Address:192.168.2.1, IP Address:10.254.1.2
+if [[ "$(grep -c "Subject Alternative Name" "$san" || true)" -gt 0 ]]; then
+	echo "PASS: ${url}"
+	exit 0
+else
+	echo "INVALID: ${url}"
+	exit 1
+fi
